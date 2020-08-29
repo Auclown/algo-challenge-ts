1cf29be1bc557883435e90fcaaf71a51beb4611e
Auclown <auclown@gmail.com>
2020-08-30 11:49:20 +1200

    30th/Aug/2020

--- 013_array-change/attempt.ts
+++ 013_array-change/attempt.ts
@@ -1,27 +1,34 @@
 const arrayChange = (a: number[]): number => {
   let count: number = 0;
-  let keepGoing = true;
+  // let keepGoing = true;
 
-  // Keep looping until the input array is sorted
-  while (keepGoing) {
+  // // Keep looping until the input array is sorted
+  // while (keepGoing) {
+  //   for (let i = 0; i < a.length - 1; i++) {
+  //     console.log(`============== Iteration: ${i} ==============`);
+  //     console.log(`============ Current count: ${count} ============`);
 
-    for (let i = 0; i < a.length - 1; i++) {
-      console.log(`============== Iteration: ${i} ==============`);
-      console.log(`============ Current count: ${count} ============`);
+  //     // Check if next element is smaller than the current one
+  //     // If so, add 1 to it and add 1 to count too.
+  //     if (a[i] >= a[i + 1]) {
+  //       a[i] += 1;
+  //       count++;
+  //     }
 
-      // Check if next element is smaller than the current one
-      // If so, add 1 to it and add 1 to count too.
-      if (a[i + 1] <= a[i]) {
-        a[i + 1] += 1;
-        count++;
-      }
+  //     // Sort the current array to see if it is sorted.
+  //     // If sorted, break the while loop.
+  //     let sorted: number[] = a.sort();
+  //     if (a === sorted) {
+  //       keepGoing = false;
+  //     }
+  //   }
+  // }
 
-      // Sort the current array to see if it is sorted.
-      // If sorted, break the while loop.
-      let sorted: number[] = a.sort();
-      if (a === sorted) {
-        keepGoing = false;
-      }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] >= a[i + 1]) {
+      const difference = a[i] + 1 - a[i + 1];
+      a[i + 1] = a[i] + 1;
+      count += difference;
     }
   }
 
